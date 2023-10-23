a762aa4970d1484acfa7ce6158e86e8991412ec3
Martin Ondrejka <martin.ondrejka@gmail.com>
2023-10-23 15:48:35 +0200

    Initial commit

---
+++ src/main/java/com/martin/socialnet/SocialNetApplication.java
@@ -0,0 +1,13 @@
+package com.martin.socialnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martin/socialnet/SocialNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.socialnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
