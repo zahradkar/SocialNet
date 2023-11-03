8516959c1c35b4f30a50545c84c3783928ffbd6b
Martin Ondrejka <martin.ondrejka@gmail.com>
2023-11-03 19:59:38 +0100

    added checking if user is logged in

---
+++ src/main/java/com/martin/socialnet/controllers/LoginController.java
@@ -0,0 +1,19 @@
+package com.martin.socialnet.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+	@GetMapping("/check-login")
+	public ResponseEntity<String> checkLogin(HttpServletRequest request) {
+		// Check if the user is logged in (you might use Spring Security or another authentication mechanism)
+		if (request.getUserPrincipal() == null)
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User is not logged in");
+		return ResponseEntity.ok("User is logged in");
+	}
+}
---
+++ src/main/java/com/martin/socialnet/security/config.java
@@ -0,0 +1,29 @@
+package com.martin.socialnet.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+//@WebSe
+public class config {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(auth -> auth
+//						.requestMatchers("/", "index.html").permitAll() // allows index.html without providing it to browser's url bar
+						.requestMatchers("/**").permitAll()
+//						.requestMatchers("/style.css").permitAll()
+//						.requestMatchers("/auth/**").permitAll()
+//						.requestMatchers("/admin/**").hasAuthority("ADMIN")
+//						.requestMatchers("/user/**").hasAnyAuthority("ADMIN", "USER")
+						.anyRequest().authenticated())
+				.formLogin(withDefaults())
+				.build();
+	}
+}
