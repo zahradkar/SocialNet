dc80ba9cc0bcef9220754dce39b0d81e002fd277
Martin Ondrejka <martin.ondrejka@gmail.com>
2023-11-03 21:35:48 +0100

    added ability to check if user is logged in + updated components accordingly

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,19 +1,51 @@
+'use strict';
 const buttons = document.querySelectorAll('button');
 
 // Iterate over the selected buttons and add event listeners
-buttons.forEach(function(button) {
-    button.addEventListener('mousedown', function() {
+buttons.forEach(function (button) {
+    button.addEventListener('mousedown', function () {
         this.classList.add('pressed');
     });
 
-    button.addEventListener('mouseup', function() {
+    button.addEventListener('mouseup', function () {
         this.classList.remove('pressed');
     });
 });
 
 // deletes image element from post__body if there is no image
-const image = document.querySelector(".post__body>img");
-console.log(image.getAttribute("src"));
+const image = document.querySelector(".post>img");
+// console.log(image.getAttribute("src"));
 // todo update condition accordingly
 if (image.getAttribute("src") === "#" || image.getAttribute("src") === "")
-    document.querySelector(".post__body").innerHTML = "";
+    document.querySelector(".post>img").style.display = 'none';
+
+
+// open login/register form after pressing login icon
+function openModal() {
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('overlay').style.display = 'none';
+}
+
+// check if user is logged in
+document.querySelectorAll('.check-login').forEach(function (button) {
+    button.addEventListener('click', function () {
+        fetch('/check-login', {
+            method: 'GET',
+            credentials: 'include'  // Include credentials (cookies) in the request
+        })
+            .then(response => {
+                if (response.ok) {
+                    // User is logged in
+                    console.log('User is logged in');
+                } else {
+                    // User is not logged in
+                    console.log('User is not logged in');
+                    openModal();
+                }
+            })
+            .catch(error => console.error('Error:', error));
+    });
+});
